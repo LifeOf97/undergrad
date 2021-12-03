c984fc8d2a026932a9d6e5b3fadd1711f03f92f5
KMA-USS <koolkatswerve@gmail.com>
2021-12-03 13:25:33 +0100

    responsive left nav up and running.

---
+++ frontend/src/components/AppStaffId.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "AppStaffId",
+    data() {
+        return {
+            // img: require("@/assets/images/staffimg.png"),
+        }
+    },
+}
+</script>
--- frontend/src/views/AuthView.vue
+++ frontend/src/views/AuthView.vue
@@ -1,22 +1,48 @@
 <template>
-    <div class="w-full h-screen flex bg-blueGray-100 p-4">
+    <div class="relative w-full h-full flex bg-blueGray-100">
+
         <!-- start of side nav -->
-        <div class="absolute top-0 left-0 py-10 w-10/12 h-screen flex flex-col items-center space-y-20 bg-blueGray-100 shadow-lg overflow-hidden">
-            <AppTextLogo />
-            <AppLeftNavLinks />
+        <div :class="nav ? 'opacity-100':'-translate-x-full opacity-0'" class="absolute top-0 left-0 w-full h-screen bg-transparent overflow-hidden backdrop-blur-lg transition-all duration-200 lg:contents lg:relative lg:backdrop-blur-none">
+            <!-- start of nav links -->
+            <div class="relative w-10/12 h-screen flex flex-col items-center space-y-20 pb-10 bg-blueGray-100 lg:w-1/5">
+                <!-- start of close btn. available on small and medium screens -->
+                <div class="absolute top-5 right-5 lg:hidden">
+                    <AppCloseButton @click.prevent="nav = false" />
+                </div>
+                <!-- end of close btn -->
+                <AppTextLogo />
+                <AppLeftNavLinks />
+            </div>
+            <!-- end of nav links -->
         </div>
         <!-- end of side nav -->
 
-        <div class="w-full bg-blue-500 rounded-lg">2</div>
+        <!-- start of main view -->
+        <div class="w-full h-screen bg-transparent p-4 md:py-4 md:pl-0 md:pr-4">
+            
+            <div class="w-full h-full bg-white rounded-2xl">
+                <button @click="nav = true">Show Nav</button>
+            </div>
+
+        </div>
+        <!-- end of main view -->
+
     </div>
 </template>
 
 <script>
 import AppTextLogo from "@/components/AppTextLogo.vue";
+import AppCloseButton from "@/components/AppCloseButton.vue";
 import AppLeftNavLinks from "@/components/AppLeftNavLinks.vue";
+
 
 export default {
     name: "AuthView",
-    components: {AppTextLogo, AppLeftNavLinks,}
+    components: {AppTextLogo, AppLeftNavLinks, AppCloseButton,},
+    data() {
+        return {
+            nav: false,
+        }
+    },
 }
 </script>
