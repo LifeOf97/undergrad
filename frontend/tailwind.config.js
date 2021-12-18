6d9a4926ddeb4d017b35b9e249cb2bb85fd4c888
KMA-USS <koolkatswerve@gmail.com>
2021-12-18 10:03:40 +0100

    Updated tailwind config.

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -11,6 +11,7 @@ module.exports = {
       },
       animation: {
         'bounce-h': 'bounce-h 1s infinite',
+        'bounce-v': 'bounce 1s infinite',
       },
       keyframes: {
         'bounce-h': {
