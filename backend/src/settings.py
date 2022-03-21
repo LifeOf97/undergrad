2ebdcf880b380aab6789c77d74b52e52f751fbc5
KMA-USS <koolkatswerve@gmail.com>
2022-03-21 14:37:49 +0100

    Updated settings.py file

--- backend/src/settings.py
+++ backend/src/settings.py
@@ -14,6 +14,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get("SECRET_KEY")
+# SECRET_KEY = CONFIG["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # Boolean value
