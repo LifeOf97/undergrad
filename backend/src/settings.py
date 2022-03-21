099e7ceaa67c7a18b48e1e96f1f6bf55db2e3a18
KMA-USS <koolkatswerve@gmail.com>
2022-03-21 11:04:57 +0100

    Updated settings.py file.

--- backend/src/settings.py
+++ backend/src/settings.py
@@ -17,7 +17,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # Boolean value
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["web-cgims.herokuapp.com",]
 
