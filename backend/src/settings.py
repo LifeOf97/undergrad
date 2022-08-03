ff4f9515c5267f936984692ea0048faecc517053
realestKMA <kelvinmayoayeni@gmail.com>
2022-08-03 15:32:26 +0100

    Update settings.py

--- backend/src/settings.py
+++ backend/src/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # Boolean value
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "web-cgims.herokuapp.com", "web-cgims.netlify.app"]
 
